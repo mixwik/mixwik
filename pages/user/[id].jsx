9a688dc766b9dd82798d560cb5bfa49d61522485
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2023-04-13 13:15:55 +0200

    minor changes in page of user

--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -1,7 +1,9 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Layout from '../../components/Layout'
-import styles from './User.module.scss'
+import { myLoader } from '../../components/myLoader'
 import { useGetOneData } from '../../firebase/hooks/getMethod/useGetOneData'
+import styles from './User.module.scss'
 
 const User = () => {
   const router = useRouter()
@@ -12,8 +14,8 @@ const User = () => {
     <Layout>
       <div className={styles.user}>
         <section className={styles.userData}>
-          <h1>Usuario</h1>
-          {user.name}
+          <Image width={0} height={0} loader={myLoader} src={user.profileImg} alt={user.name} />
+          <h1>{user.name}</h1>
         </section>
       </div>
     </Layout>
