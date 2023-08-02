9844103db401147f109b4fddc519b2741478b3bc
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2023-08-02 13:18:18 +0200

    add page extension

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos', 'lh3.googleusercontent.com', 'firebasestorage.googleapis.com']
   },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   webpack (config, { isServer }) {
     const prefix = config.assetPrefix ?? config.basePath ?? ''
     config.module.rules.push({
