58a4eae6a42417aae18d5767555feadd8964217e
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-02-11 13:53:21 +0100

    Update TitlePage component height

--- pages/juegos/components/TitlePage.jsx
+++ pages/juegos/components/TitlePage.jsx
@@ -3,7 +3,7 @@ import { myLoader } from '../../../components/myLoader'
 
 const TitlePage = ({ title, alt, image }) => {
   return (
-    <h1 className='flex justify-center h-20 p-1 md:p-2'>
+    <h1 className='flex justify-center h-10 p-1 md:h-20 md:p-2'>
       <Image className='object-cover w-auto h-full' loader={myLoader} width={80} height={100} src={image} alt={alt} />
       <span className='hidden'>{title}</span>
     </h1>
