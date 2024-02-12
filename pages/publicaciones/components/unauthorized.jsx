4b9ad415583e2ed4535fa324af72bda521f804a2
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-02-12 16:03:33 +0100

    Update package.json and unauthorized.jsx

--- pages/publicaciones/components/unauthorized.jsx
+++ pages/publicaciones/components/unauthorized.jsx
@@ -9,7 +9,7 @@ const Unauthorized = () => {
     <Layout>
       <section className='flex items-center justify-center bg-white'>
         <div className='container flex items-center justify-center px-6 py-12 mx-auto h-[90vh]'>
-          <div className='flex flex-col items-center max-w-sm mx-auto text-center'>
+          <div className='flex flex-col items-center max-w-lg mx-auto'>
             <svg xmlns='http://www.w3.org/2000/svg' width='44' height='44' viewBox='0 0 24 24' stroke-width='1.5' stroke='#ffffff' fill='none' stroke-linecap='round' stroke-linejoin='round'>
               <path stroke='none' d='M0 0h24v24H0z' fill='none' />
               <path d='M4 8v-2a2 2 0 0 1 2 -2h2' />
@@ -19,9 +19,16 @@ const Unauthorized = () => {
               <path d='M8 11m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v3a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z' />
               <path d='M10 11v-2a2 2 0 1 1 4 0v2' />
             </svg>
-            <h1 className='mt-3 text-2xl font-semibold text-gray-800 md:text-3xl'>No puedes acceder a este contenido</h1>
-            <p className='mt-4 text-gray-500'>Si quieres ver las publicaciones de los usuarios debes de estar registrado en MixWik.com</p>
-
+            <h1 className='mt-3 text-2xl font-semibold text-gray-800 md:text-3xl text-pretty'>No puedes acceder a este contenido</h1>
+            <p className='mt-4 text-gray-500 text-balance'>Si quieres ver las publicaciones de los usuarios debes de estar registrado en MixWik.com.</p>
+            <p className='mt-4 text-left text-gray-500 text-balance'>Regístrate para formar parte de una gran comunidad de jugadores, en MixWik podrás:</p>
+            <ul className='mt-4 text-left text-gray-500'>
+              <li>Publicar tus propias publicaciones.</li>
+              <li>Encontrar jugadores cercanos.</li>
+              <li>Guarda tus jugadores favoritos.</li>
+              <li>Crear y buscar equipos para llegar a lo más alto en tu juego favorito.</li>
+              <li>Y mucho más...</li>
+            </ul>
             <div className='flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto'>
               <Link href='/' className='flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100'>
                 <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' className='w-5 h-5 rtl:rotate-180'>
