32e259a1544ba309d65921e87bba17468b37a27a
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-02-13 10:19:08 +0100

    Refactor useGamesFilters to improve filtering logic

--- hooks/useGamesFilters/index.jsx
+++ hooks/useGamesFilters/index.jsx
@@ -1,46 +1,25 @@
 import { useFilterContext } from '../../context'
 import { useUserFilterDistance } from '../useUsersFilterDistance'
+import { useCallback } from 'react'
 
 export const useGamesFilters = (user, DB, distance) => {
   const filter = useFilterContext()
+  let users = useUserFilterDistance(user, DB, distance)
 
-  const listUsers = useUserFilterDistance(user, DB, distance)
+  const filterUsers = useCallback((users, filter, key) => {
+    if (filter[key].length) {
+      return users.filter(user => filter[key].some(filterValue => user[key].includes(filterValue)))
+    }
+    return users
+  }, [])
 
-  const listUsersPosition = filter.position.length
-    ? listUsers.filter(fil => {
-      return filter.position.some((fil2) => {
-        return fil.position.includes(fil2)
-      })
-    })
-    : listUsers
+  const filterKeys = ['position', 'typeOfGamer', 'level', 'preferenceTeam']
 
-  const listUsersTypeOfGamer = filter.typeOfGamer.length
-    ? listUsersPosition.filter(fil => {
-      return filter.typeOfGamer.some((fil2) => {
-        return fil.typeOfGamer.includes(fil2)
-      })
-    })
-    : listUsersPosition
+  filterKeys.forEach(key => {
+    users = filterUsers(users, filter, key)
+  })
 
-  const listUsersLevel = filter.level.length
-    ? listUsersTypeOfGamer.filter(fil => {
-      return filter.level.some((fil2) => {
-        return fil.level.includes(fil2)
-      })
-    })
-    : listUsersTypeOfGamer
+  users = users.filter(user => user.age >= filter.age.min && user.age <= filter.age.max)
 
-  const listUsersPreferenceTeam = filter.preferenceTeam.length
-    ? listUsersLevel.filter(fil => {
-      return filter.preferenceTeam.some((fil2) => {
-        return fil.preferenceTeam.includes(fil2)
-      })
-    })
-    : listUsersLevel
-
-  const listUsersAge = listUsersPreferenceTeam.filter(fil => fil.age >= filter.age.min && fil.age <= filter.age.max)
-
-  const usersFiltered = listUsersAge
-
-  return usersFiltered
+  return users
 }
