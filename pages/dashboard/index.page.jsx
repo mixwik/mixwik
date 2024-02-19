dc2fdd8b4cff4207d2a49ac5318ba47148a5aabb
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-02-19 08:15:20 +0100

    Refactor user profile image rendering

--- pages/dashboard/index.page.jsx
+++ pages/dashboard/index.page.jsx
@@ -91,13 +91,24 @@ export default function Dashboard () {
               data-isActive={page === 'profile'}
               onClick={() => handleClick('profile')}
             >
-              <Image
-                width={0}
-                height={0}
-                loader={myLoader}
-                src={userProvider?.image}
-                alt={userProvider?.name}
-              />
+              {
+              userProvider?.image
+                ? (
+                  <Image
+                    width={0}
+                    height={0}
+                    loader={myLoader}
+                    src={userProvider?.image}
+                    alt={userProvider?.name}
+                  />
+                  )
+                : (
+                  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-10 h-10'>
+                    <path fillRule='evenodd' d='M12 2a3 3 0 100 6 3 3 0 000-6zm-5 3a5 5 0 019.858 1.716c.03.1.142.284.142.284a.75.75 0 01-.142.284A5 5 0 017 5H4a1 1 0 00-1 1v12a1 1 0 001 1h3a5 5 0 010-10zm10 10a5 5 0 110-10 5 5 0 010 10z' clipRule='evenodd' />
+                  </svg>
+
+                  )
+            }
               Perfil
             </li>
             <li
