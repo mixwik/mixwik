066d2d28b0812b20fdea3033705fbf3e5769341c
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-02-20 23:25:37 +0100

    Refactor useLogInProvider function and add setLogInOpen context

--- firebase/auth/useLogIn/index.js
+++ firebase/auth/useLogIn/index.js
@@ -1,15 +1,18 @@
-import { useState } from 'react'
 import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { useLogInOpenContext } from '../../../context'
 import { auth } from '../../initialize'
 
 export const useLogInProvider = () => {
+  const { setLogInOpen } = useLogInOpenContext()
   const [error, setError] = useState(false)
   const router = useRouter()
   const logInProvider = (provider) => {
     signInWithPopup(auth, provider)
       .then(() => {
         router.push('/dashboard?page=profile')
+        setLogInOpen(false)
       }).catch((error) => {
         setError(error)
       })
