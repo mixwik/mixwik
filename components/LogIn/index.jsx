0927fd4c42382a66b2a89b7cf52639303489497c
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-02-21 12:34:05 +0100

    Fix SVG stroke attributes

--- components/LogIn/index.jsx
+++ components/LogIn/index.jsx
@@ -36,7 +36,7 @@ export const LogIn = () => {
       <button onClick={() => setLogInOpen(false)} type='button' className='absolute z-10 inline-flex items-center justify-center p-2 text-gray-400 rounded-full bg-pennBlue hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-aero md:right-96 md:top-10 right-2 top-2'>
         <span className='sr-only'>Close menu</span>
         <svg className='w-6 h-6 text-aero' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-          <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
         </svg>
       </button>
       <div className='relative h-[90vh] flex flex-col justify-center items-center bg-gray-100 md:px-0 px-5'>
--- pages/desautorizado/index.page.tsx
+++ pages/desautorizado/index.page.tsx
@@ -9,7 +9,7 @@ const Unauthorized = () => {
       <section className='flex items-center justify-center bg-white'>
         <div className='container flex items-center justify-center px-6 py-12 mx-auto h-[90vh]'>
           <div className='flex flex-col items-center max-w-lg mx-auto'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='44' height='44' viewBox='0 0 24 24' stroke-width='1.5' stroke='#ffffff' fill='none' stroke-linecap='round' stroke-linejoin='round'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='44' height='44' viewBox='0 0 24 24' strokeWidth='1.5' stroke='#ffffff' fill='none' strokeLinecap='round' strokeLinejoin='round'>
               <path stroke='none' d='M0 0h24v24H0z' fill='none' />
               <path d='M4 8v-2a2 2 0 0 1 2 -2h2' />
               <path d='M4 16v2a2 2 0 0 0 2 2h2' />
@@ -30,8 +30,8 @@ const Unauthorized = () => {
             </ul>
             <div className='flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto'>
               <Link href='/' className='flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100'>
-                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' className='w-5 h-5 rtl:rotate-180'>
-                  <path stroke-linecap='round' stroke-linejoin='round' d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18' />
+                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-5 h-5 rtl:rotate-180'>
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18' />
                 </svg>
                 Volver Inicio
               </Link>
