7b49a7c35d24e4ca29e9946f8647042cf815220c
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-02-26 12:57:46 +0100

    Add COLLECTIONS dictionary

--- pages/juegos/cs2/index.page.jsx
+++ pages/juegos/cs2/index.page.jsx
@@ -3,13 +3,14 @@ import { FilterCs2 } from '../../../components/Filters/cs2'
 import Layout from '../../../components/Layout'
 import PageContent from '../components/PageContent'
 import TitlePage from '../components/TitlePage'
+import { COLLECTIONS } from '../../../domain/constants'
 
 const Csgo = () => {
   const [distance, setDistance] = useState(700)
   return (
     <Layout>
       <PageContent
-        category='cs2'
+        category={COLLECTIONS.cs2}
         distance={distance}
       >
         <FilterCs2 distance={distance} setDistance={setDistance} />
