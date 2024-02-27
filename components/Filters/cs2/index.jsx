629997ec0adfcb876f2c41ef34ed0412a0a78d76
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-02-27 13:07:45 +0100

    Add DistanceFilter component to FilterCs2

--- components/Filters/cs2/index.jsx
+++ components/Filters/cs2/index.jsx
@@ -6,30 +6,14 @@ import TypeOfGamer from '../../Forms/TypeOfGamer'
 import { FilterCancelIcon, FilterIcon } from '../../Svg'
 import { LevelFilter } from '../components/LevelFilter'
 import { AgeFilter } from '../components/ageFilter'
+import { DistanceFilter } from '../components/distanceFilter'
 
 export const FilterCs2 = ({ distance, setDistance }) => {
   const [openFilters, setOpenFilters] = useState(false)
   return (
     <section className='relative flex flex-col gap-5 bg-white'>
       <div className='flex justify-evenly items-center z-30 h-[5vh] bg-white'>
-        <div className='flex items-center gap-2'>
-          <label className='font-semibold text-slate-900'>
-            Distancia
-          </label>
-          <input
-            id='steps-range'
-            step='5'
-            type='range'
-            min='1'
-            max='700'
-            onChange={(e) => setDistance(e.target.value)}
-            value={distance}
-            className='w-full h-3 bg-gray-100 shadow-md shadow-gray-300 rounded-lg appearance-none cursor-pointer [&::-webkit-slider-thumb]:bg-aero [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:size-5 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:shadow-lg'
-          />
-          <span className='z-10 bottom-4 right-2'>
-            {distance > 0 ? distance : 0}Km
-          </span>
-        </div>
+        <DistanceFilter distance={distance} setDistance={setDistance} />
         <button
           className={`flex items-center gap-1 ${openFilters && 'text-aero'}`}
           onClick={() => setOpenFilters(prev => !prev)}
