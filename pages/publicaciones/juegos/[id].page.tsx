244e70c85218d6175bd55efea39602247bed0d2a
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-04-04 09:42:56 +0200

    Remove Carousel component and update ImagesCarousel usage

--- pages/publicaciones/juegos/[id].page.tsx
+++ pages/publicaciones/juegos/[id].page.tsx
@@ -1,13 +1,15 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import { Carousel } from 'react-responsive-carousel'
 import Layout from '../../../components/Layout'
 import UserMap from '../../../components/UserMap'
 import { BackgroundDots } from '../../../components/background-dots'
+import { myLoader } from '../../../components/myLoader'
 import { useSession } from '../../../firebase/auth/useSession'
 import { useGetOneUser } from '../../../hooks/use-get-one-user'
 import { useMixWikTeamsCheckSubscription } from '../../../hooks/useChecksStripe'
 import { useLimitedAdministrator } from '../../../hooks/useLimitedAdministrator'
-import { ImagesCarousel } from '../components/carousel'
 import { Edit } from '../components/edit'
 import { Description } from '../components/fields/description'
 import { Header } from '../components/fields/header'
@@ -16,6 +18,7 @@ import { Player } from '../components/player'
 import { Team } from '../components/team'
 import { useGetGameCategory } from '../hooks/use-get-game-category'
 import { useImages } from '../hooks/use-images'
+import styles from './Carouse.module.scss'
 
 const PublicationsGamesPage = () => {
   const [edit, setEdit] = useState(false)
@@ -44,7 +47,21 @@ const PublicationsGamesPage = () => {
             setEdit={setEdit}
           />
           <div className='flex flex-col gap-10 p-3 pt-0 md:p-5 md:pt-0'>
-            <ImagesCarousel images={images} />
+            <div className={styles.imgBox}>
+              <Carousel
+                className={styles.carousel}
+                showStatus={false}
+                showThumbs={false}
+              >
+                {
+                images
+                  .filter(image => image?.url !== '')
+                  .map((image, index) => (
+                    <Image key={index} loader={myLoader} width={100} height={100} src={image.url} alt={image.name} />
+                  ))
+                }
+              </Carousel>
+            </div>
             <Title title={publication?.title} />
             <Description description={publication?.description} />
             <Team field={publication} page={page} />
