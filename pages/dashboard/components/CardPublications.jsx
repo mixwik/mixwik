a2b7c6779bb89cd9643c81051456fc1ee92c16ac
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-07-16 12:36:53 +0200

    delete option of eliminate de main publication

--- pages/dashboard/components/CardPublications.jsx
+++ pages/dashboard/components/CardPublications.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import { myLoader } from '../../../components/myLoader'
 import { COLLECTIONS, PUBLICATION_TYPE } from '../../../domain/constants'
 import { deletePublication } from '../../../firebase/hooks/deleteMethod'
-import { removeFavorites } from '../hooks/remove-favorites'
 import { useMixWikTeamsCheckSubscription } from '../../../hooks/useChecksStripe'
+import { removeFavorites } from '../hooks/remove-favorites'
 
 const CardPublications = ({ publication, equip, updateFav, remove, user }) => {
   const category = equip ? COLLECTIONS.teams : publication.category
@@ -25,11 +25,11 @@ const CardPublications = ({ publication, equip, updateFav, remove, user }) => {
     }
   }
   return (
-    <section className='flex flex-col justify-between h-40 overflow-hidden bg-gray-100 rounded-md shadow-xl w-80'>
+    <section className='relative flex flex-col justify-between h-40 overflow-hidden bg-gray-100 rounded-md shadow-xl w-80'>
       {
         !isMixWikTeams && (publication.type === PUBLICATION_TYPE.playerWithTeam || publication.type === PUBLICATION_TYPE.team)
           ? (
-            <div className='relative flex items-center justify-center h-32 size-full'>
+            <div className='relative flex items-center justify-center h-40'>
               <Image className='object-cover w-full h-full' loader={myLoader} width={0} height={0} src={publication.img.url} alt={publication.title} />
               <h2 className='absolute top-0 left-0 z-10 p-1 font-bold text-white bg-black'>{publication.title.slice(0, 15)}...</h2>
               <p className='absolute z-20 flex items-center justify-center w-full h-full text-xl font-bold text-white bg-black/50'>Inactivo</p>
@@ -37,19 +37,22 @@ const CardPublications = ({ publication, equip, updateFav, remove, user }) => {
             </div>
             )
           : (
-            <Link className='flex items-center justify-center h-32 size-full' href={`/publicaciones/juegos/${publication?.id}?type=${publication?.type}&category=${publication?.category}`}>
+            <Link className='flex items-center justify-center h-40' href={`/publicaciones/juegos/${publication?.id}?type=${publication?.type}&category=${publication?.category}`}>
               <div className='relative w-full h-full text-white'>
                 <Image className='object-cover w-full h-full' loader={myLoader} width={0} height={0} src={publication.img.url} alt={publication.title} />
-                <h2 className='absolute top-0 left-0 z-50 p-1 font-bold bg-black'>{publication.title.slice(0, 15)}...</h2>
+                <h2 className='absolute top-0 left-0 z-50 p-1 font-bold bg-black'>{publication.title}</h2>
               </div>
             </Link>
             )
       }
-      <button onClick={handleClick} className='h-8 font-semibold text-white bg-pennBlue'>
-        {
+      {
+      publication.type !== PUBLICATION_TYPE.player &&
+        <button onClick={handleClick} className='absolute bottom-0 right-0 w-full h-8 font-semibold text-white bg-pennBlue'>
+          {
           remove ? 'Eliminar' : 'Quitar favorito'
         }
-      </button>
+        </button>
+      }
     </section>
   )
 }
