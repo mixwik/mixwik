c5f5cd6ef98aadb8fddb87ac0f6a66ba56956edb
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2025-04-21 11:27:43 +0200

    Fix regex of discord

--- domain/regex.ts
+++ domain/regex.ts
@@ -1,5 +1,5 @@
 export const REGEX = {
-  discord: /^(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li|com)\/|discordapp\.com\/invite\/)([a-zA-Z0-9-]{2,32})$/,
+  discord: /discord/i,
   twitterOrX: /^(https?:\/\/)?(www\.)?x\.com\/([a-zA-Z0-9_]{1,15})$|^(https?:\/\/)?(www\.)?twitter\.com\/(#!\/)?[a-zA-Z0-9_]{1,15}(\/\w+)*$/,
   instagram: /^https?:\/\/(www\.)?instagram\.com\/[a-zA-Z0-9_]+$/,
   facebook: /^https?:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9_]+$/,
