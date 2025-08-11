bc24af4c94057817f73feca9a7acc72fffeca59c
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2025-08-11 12:02:34 +0200

    changes in update-user-data

--- pages/api/update-user-data/index.page.ts
+++ pages/api/update-user-data/index.page.ts
@@ -1,107 +1,27 @@
-import { differenceInYears, parseISO } from 'date-fns'
 import { doc, updateDoc } from 'firebase/firestore'
 import { NextApiRequest, NextApiResponse } from 'next'
-import * as yup from 'yup'
 import { COLLECTIONS } from '../../../domain/constants'
-import { REGEX } from '../../../domain/regex'
 import { db } from '../../../firebase/initialize'
-
-const schema = yup
-  .object({
-    age: yup
-      .string()
-      .required('La fecha de nacimiento es obligatorio')
-      .test('is-over-16', 'Debes ser mayor de 16 años.', function (value) {
-        return differenceInYears(new Date(), parseISO(value)) >= 16
-      }),
-    description: yup
-      .string()
-      .required('El campo descripción es obligatorio')
-      .min(100, 'Mínimo 100 caracteres')
-      .max(350, 'Máximo 350 caracteres')
-  })
-  .shape({
-    twitter: yup
-      .string()
-      .notRequired()
-      .when('twitter', {
-        is: (value: string) => value?.length,
-        then: (rule) => rule.matches(REGEX.twitterOrX, 'La URL de twitter/X no es valida')
-      }),
-    discord: yup
-      .string()
-      .notRequired()
-      .when('discord', {
-        is: (value: string) => value?.length,
-        then: (rule) => rule.matches(REGEX.discord, 'La URL de discord no es valida')
-      }),
-    instagram: yup
-      .string()
-      .notRequired()
-      .when('instagram', {
-        is: (value: string) => value?.length,
-        then: (rule) => rule.matches(REGEX.instagram, 'La URL de instagram no es valida')
-      }),
-    facebook: yup
-      .string()
-      .notRequired()
-      .when('facebook', {
-        is: (value: string) => value?.length,
-        then: (rule) => rule.matches(REGEX.facebook, 'La URL de facebook no es valida')
-      }),
-    twitch: yup
-      .string()
-      .notRequired()
-      .when('twitch', {
-        is: (value: string) => value?.length,
-        then: (rule) => rule.matches(REGEX.twitch, 'La URL de twitch no es valida')
-      }),
-    youtube: yup
-      .string()
-      .notRequired()
-      .when('youtube', {
-        is: (value: string) => value?.length,
-        then: (rule) => rule.matches(REGEX.youtube, 'La URL de youtube no es valida')
-      }),
-    tiktok: yup
-      .string()
-      .notRequired()
-      .when('tiktok', {
-        is: (value: string) => value?.length,
-        then: (rule) => rule.matches(REGEX.tiktok, 'La URL de tiktok no es valida')
-      })
-  }, [['twitter', 'twitter'], ['discord', 'discord'], ['instagram', 'instagram'], ['facebook', 'facebook'], ['twitch', 'twitch'], ['youtube', 'youtube'], ['tiktok', 'tiktok']])
-  .test(
-    'at-least-one-input',
-    'Al menos uno de los dos campos debe estar lleno',
-    obj => !!obj.twitter || !!obj.discord || !!obj.instagram || !!obj.facebook || !!obj.twitch || !!obj.youtube || !!obj.tiktok
-  )
-  .required()
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    const { uid, age, description, twitter, discord, instagram, youtube, tiktok, facebook, twitch, imageUrl, imageName } = req.body
-    try {
-      await schema.validate(req.body)
-    } catch (error) {
-      return res.status(400).json({ error: error.message })
-    }
-    const userRef = doc(db, COLLECTIONS.users, uid)
+    const data = JSON.parse(req.body)
+    const userRef = doc(db, COLLECTIONS.users, data.uid)
     await updateDoc(userRef, {
-      description,
-      age,
+      description: data.description,
+      age: data.age,
       profileImg: {
-        url: imageUrl,
-        name: imageName
+        url: data.imageUrl,
+        name: data.imageName
       },
       social: {
-        twitter,
-        discord,
-        instagram,
-        youtube,
-        tiktok,
-        facebook,
-        twitch
+        twitter: data.twitter,
+        discord: data.discord,
+        instagram: data.instagram,
+        youtube: data.youtube,
+        tiktok: data.tiktok,
+        facebook: data.facebook,
+        twitch: data.twitch
       }
     }).then(() => {
       res.status(200).json({ message: 'update' })
